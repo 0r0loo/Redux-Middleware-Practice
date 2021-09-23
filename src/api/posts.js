9eb9e3d26755fd569cbcbc574b3f4093c459f906
chotaegyu <chotg5592@gmail.com>
2021-09-23 20:48:44 +0900

    REST API

--- src/api/posts.js
+++ src/api/posts.js
@@ -1,29 +1,11 @@
-const sleep = (n) => new Promise((resolve) => setTimeout(resolve, n));
-
-const posts = [
-  {
-    id: 1,
-    title: '리덕스 미들웨어 배우기',
-    body: '리덕스 미들웨어 직접 만들어보기',
-  },
-  {
-    id: 2,
-    title: '리덕스 덩크를 사용해봅시다.',
-    body: '리덕스 덩크를 사용해 비동기 작업을 처리해봅시다.',
-  },
-  {
-    id: 3,
-    title: '리덕스 사가를 사용해봅시다.',
-    body: '리덕스 사가를 사용해 비동기 작업을 처리해봅시다.',
-  },
-];
+import axios from 'axios';
 
 export const getPosts = async () => {
-  await sleep(500);
-  return posts;
+  const response = await axios.get('http://localhost:4000/posts');
+  return response.data;
 };
 
 export const getPostsById = async (id) => {
-  await sleep(500);
-  return posts.find((post) => post.id === id);
+  const response = await axios.get(`http://localhost:4000/posts/${id}`);
+  return response.data;
 };
