8b39c884d6d475603622011c5cc1e0976e133ea8
chotaegyu <chotg5592@gmail.com>
2021-09-23 20:28:19 +0900

    thunk함수에서 history 사용하는 방법

--- src/index.js
+++ src/index.js
@@ -7,18 +7,27 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './modules';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import ReduxThunk from 'redux-thunk';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
+import { Router } from 'react-router';
+import { createBrowserHistory } from 'history';
+
+const customHistory = createBrowserHistory();
 
 const store = createStore(
   rootReducer,
-  composeWithDevTools(applyMiddleware(ReduxThunk, logger))
+  composeWithDevTools(
+    applyMiddleware(
+      ReduxThunk.withExtraArgument({ history: customHistory }),
+      logger
+    )
+  )
 );
 
 ReactDOM.render(
-  <BrowserRouter>
+  <Router history={customHistory}>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>,
+  </Router>,
   document.getElementById('root')
 );
